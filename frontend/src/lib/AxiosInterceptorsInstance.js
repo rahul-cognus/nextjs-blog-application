18497a9b038599fe3712713c5f8a6831534d11a4
Rahul Jangid <rahul.jangid.cognus@gmail.com>
2024-09-27 13:03:16 +0530

    Added axios Instance

---
+++ frontend/src/lib/AxiosInterceptorsInstance.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+import { cookies } from "next/headers";
+
+// Create an instance of axios with the base URL
+const axiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+// Request interceptor to include token in Authorization header
+axiosInstance.interceptors.request.use(
+    async (config) => {
+        try {
+            const cookieData = await getCookieData();
+            if (cookieData) {
+                const tokenCookie = cookieData.find(cookie => cookie.name === '_token');
+                const accessToken = tokenCookie?.value;
+                if (accessToken) {
+                    config.headers.Authorization = `Bearer ${accessToken}`;
+                }
+            }
+        } catch (error) {
+            console.error("Request interceptor error - fetching cookie data failed:", error);
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
+
+// get all cookie
+async function getCookieData() {
+    try {
+        const cookieStore = cookies();
+        return cookieStore.getAll();
+    } catch (error) {
+        throw new Error(`Failed to retrieve cookies: ${error.message}`);
+    }
+}
