355e3c99bfefefd62f5d064407df0240f80ce0ac
Himanshu.suthar <rahul.jangid.cognus@gmail.com>
2024-10-03 00:27:29 +0530

    Changes

--- frontend/src/app/(website)/[categories]/page.jsx
+++ frontend/src/app/(website)/[categories]/page.jsx
@@ -1,79 +1,154 @@
-import BreadCrumb from '@/components/Homepage/BreadCrumb'
-import TrendingTopic from '@/components/Homepage/TrendingTopic'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import BreadCrumb from "@/components/Homepage/BreadCrumb";
+import TrendingTopic from "@/components/Homepage/TrendingTopic";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const cardData = [
-    {imageSrc : "/images/blog/4by3/01.jpg",category:"Technology",categoryStyle:"bg-warning text-black [&>span]:bg-black",isSponsored:true,title:"12 worst types of business accounts you follow on Twitter",
-        para:"He moonlights difficult engrossed it, sportsmen. Interested has all Devonshire difficulty gay assistance joy. Unaffected at ye of compliment alteration to",
-        authorImg:"/images/avatar/01.jpg",authorName:"Samuel",date:"Jan 22, 2022"
-    },
-    {imageSrc : "/images/blog/4by3/02.jpg",category:"Travel",categoryStyle:"bg-red-500 text-white [&>span]:bg-white",isSponsored:false,title:"Dirty little secrets about the business industry",
-        para:"Place voice no arises along to. Parlors waiting so against me no. Wishing calling is warrant settled was lucky. Express besides it present if at an opinion visitor.",
-        authorImg:"/images/avatar/02.jpg",authorName:"Dennis",date:"Jan 22, 2022"
-    },
-    {imageSrc : "/images/blog/4by3/03.jpg",category:"Gadget",categoryStyle:"bg-success text-white [&>span]:bg-white",isSponsored:false,title:"Bad habits that people in the industry need to quit",
-        para:"For who thoroughly her boy estimating conviction. Removed demands expense account in outward tedious do. Particular way thoroughly unaffected",
-        authorImg:"/images/avatar/03.jpg",authorName:"Bryan",date:"Jan 22, 2022"
-    },
-    {imageSrc : "/images/blog/4by3/04.jpg",category:"Sports",categoryStyle:"bg-primary text-white [&>span]:bg-white",isSponsored:false,title:"Around the web: 20 fabulous infographics about business",
-        para:"Projection favorable Mrs can be projecting own. Thirty it matter enable become admire in giving. See resolved goodness felicity shy civility domestic had but.",
-        authorImg:"/images/avatar/04.jpg",authorName:"Billy",date:"Jan 22, 2022"
-    },
-    {imageSrc : "/images/blog/4by3/05.jpg",category:"",categoryStyle:"",isSponsored:false,title:"7 common mistakes everyone makes while traveling",
-        para:"Drawings offended yet answered Jennings perceive laughing six did far. Rooms oh fully taken by worse do. Points afraid but may end law lasted.",
-        authorImg:"/images/avatar/05.jpg",authorName:"Jacqueline",date:"Jan 22, 2022"
-    },
-    {imageSrc : "/images/blog/4by3/06.jpg",category:"Photography",categoryStyle:"bg-red-500 text-white [&>span]:bg-white",isSponsored:false,title:"5 investment doubts you should clarify",
-        para:"Was out laughter raptures returned outweigh. Luckily cheered colonel I do we attack highest enabled. Tried law yet style child. The bore of true of no be deal.",
-        authorImg:"/images/avatar/06.jpg",authorName:"Carolyn",date:"Jan 22, 2022"
-    }
-]
-const page = () => {
+  {
+    imageSrc: "/images/blog/4by3/01.jpg",
+    category: "Technology",
+    categoryStyle: "bg-warning text-black [&>span]:bg-black",
+    isSponsored: true,
+    title: "12 worst types of business accounts you follow on Twitter",
+    para: "He moonlights difficult engrossed it, sportsmen. Interested has all Devonshire difficulty gay assistance joy. Unaffected at ye of compliment alteration to",
+    authorImg: "/images/avatar/01.jpg",
+    authorName: "Samuel",
+    date: "Jan 22, 2022",
+  },
+  {
+    imageSrc: "/images/blog/4by3/02.jpg",
+    category: "Travel",
+    categoryStyle: "bg-red-500 text-white [&>span]:bg-white",
+    isSponsored: false,
+    title: "Dirty little secrets about the business industry",
+    para: "Place voice no arises along to. Parlors waiting so against me no. Wishing calling is warrant settled was lucky. Express besides it present if at an opinion visitor.",
+    authorImg: "/images/avatar/02.jpg",
+    authorName: "Dennis",
+    date: "Jan 22, 2022",
+  },
+  {
+    imageSrc: "/images/blog/4by3/03.jpg",
+    category: "Gadget",
+    categoryStyle: "bg-success text-white [&>span]:bg-white",
+    isSponsored: false,
+    title: "Bad habits that people in the industry need to quit",
+    para: "For who thoroughly her boy estimating conviction. Removed demands expense account in outward tedious do. Particular way thoroughly unaffected",
+    authorImg: "/images/avatar/03.jpg",
+    authorName: "Bryan",
+    date: "Jan 22, 2022",
+  },
+  {
+    imageSrc: "/images/blog/4by3/04.jpg",
+    category: "Sports",
+    categoryStyle: "bg-primary text-white [&>span]:bg-white",
+    isSponsored: false,
+    title: "Around the web: 20 fabulous infographics about business",
+    para: "Projection favorable Mrs can be projecting own. Thirty it matter enable become admire in giving. See resolved goodness felicity shy civility domestic had but.",
+    authorImg: "/images/avatar/04.jpg",
+    authorName: "Billy",
+    date: "Jan 22, 2022",
+  },
+  {
+    imageSrc: "/images/blog/4by3/05.jpg",
+    category: "",
+    categoryStyle: "",
+    isSponsored: false,
+    title: "7 common mistakes everyone makes while traveling",
+    para: "Drawings offended yet answered Jennings perceive laughing six did far. Rooms oh fully taken by worse do. Points afraid but may end law lasted.",
+    authorImg: "/images/avatar/05.jpg",
+    authorName: "Jacqueline",
+    date: "Jan 22, 2022",
+  },
+  {
+    imageSrc: "/images/blog/4by3/06.jpg",
+    category: "Photography",
+    categoryStyle: "bg-red-500 text-white [&>span]:bg-white",
+    isSponsored: false,
+    title: "5 investment doubts you should clarify",
+    para: "Was out laughter raptures returned outweigh. Luckily cheered colonel I do we attack highest enabled. Tried law yet style child. The bore of true of no be deal.",
+    authorImg: "/images/avatar/06.jpg",
+    authorName: "Carolyn",
+    date: "Jan 22, 2022",
+  },
+];
+const Category = () => {
   return (
-    <div className='container'>
-        <BreadCrumb/>
-        <div className='flex flex-col sm:flex-row items-start gap-5 '>
-            <div className='w-full sm:w-3/4'>
+    <div className="container">
+      <BreadCrumb />
+      <div className="flex flex-col sm:flex-row items-start gap-5 ">
+        <div className="w-full sm:w-3/4">
+          <>
+            {cardData.map((item, index) => {
+              return (
                 <>
-                    {cardData.map((item,index)=>{
-                        return(
-                            <>
-                                <div key={index} className="bg-white rounded-xl overflow-hidden grow">
-                                {/* <!-- Card img --> */}
-                                <div className="relative rounded-xl overflow-hidden">
-                                    <Image width={600} height={450} className="w-full" src={item.imageSrc} alt="Card image" />
-                                    {item.category && 
-                                        <div className="absolute bottom-0 flex flex-col justify-between p-3">
-                                            <Link href="#" className={`flex items-center gap-2 text-xs px-2 py-1 rounded ${item.categoryStyle}`}><span className='size-2 flex rounded-full'></span>{ item.category}</Link>
-                                        </div>
-                                    }
-                                </div>
-                                <div className="py-3">
-                                    <h4 className="text-2xl font-bold text-[#191A1F]"><Link href="post-single.html" className=" btn-link">{item.title}</Link></h4>
-                                    <p className="text-gray-700">{item.para}</p>
-                                    {/* <!-- Card info --> */}
-                                    <ul className="flex items-center space-x-2 mt-4 text-sm">
-                                    <li className="flex items-center">
-                                        <Image width={100} height={100} className="w-8 h-8 rounded-full" src={item.authorImg} alt="avatar" />
-                                        <span className="ml-2">by <Link href="#" className="text-blue-500 hover:underline">{item.authorName}</Link></span>
-                                    </li>
-                                    <li className="ml-auto">{item.date}</li>
-                                    </ul>
-                                </div>
-                                </div>
-                            </>
-                        )
-                    })}
+                  <div
+                    key={index}
+                    className="bg-white rounded-xl overflow-hidden grow"
+                  >
+                    {/* <!-- Card img --> */}
+                    <div className="relative rounded-xl overflow-hidden">
+                      <Image
+                        width={600}
+                        height={450}
+                        className="w-full"
+                        src={item.imageSrc}
+                        alt="Card image"
+                      />
+                      {item.category && (
+                        <div className="absolute bottom-0 flex flex-col justify-between p-3">
+                          <Link
+                            href="#"
+                            className={`flex items-center gap-2 text-xs px-2 py-1 rounded ${item.categoryStyle}`}
+                          >
+                            <span className="size-2 flex rounded-full"></span>
+                            {item.category}
+                          </Link>
+                        </div>
+                      )}
+                    </div>
+                    <div className="py-3">
+                      <h4 className="text-2xl font-bold text-[#191A1F]">
+                        <Link href="post-single.html" className=" btn-link">
+                          {item.title}
+                        </Link>
+                      </h4>
+                      <p className="text-gray-700">{item.para}</p>
+                      {/* <!-- Card info --> */}
+                      <ul className="flex items-center space-x-2 mt-4 text-sm">
+                        <li className="flex items-center">
+                          <Image
+                            width={100}
+                            height={100}
+                            className="w-8 h-8 rounded-full"
+                            src={item.authorImg}
+                            alt="avatar"
+                          />
+                          <span className="ml-2">
+                            by{" "}
+                            <Link
+                              href="#"
+                              className="text-blue-500 hover:underline"
+                            >
+                              {item.authorName}
+                            </Link>
+                          </span>
+                        </li>
+                        <li className="ml-auto">{item.date}</li>
+                      </ul>
+                    </div>
+                  </div>
                 </>
-            </div>
-            <div className='w-full sm:w-1/4 sticky top-0'>
-                <TrendingTopic title={'Other Categories'}/>
-            </div>
+              );
+            })}
+          </>
         </div>
+        <div className="w-full sm:w-1/4 sticky top-0">
+          <TrendingTopic title={"Other Categories"} />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default Category;
--- frontend/src/components/dashboard/layout/HeaderDashboard.jsx
+++ frontend/src/components/dashboard/layout/HeaderDashboard.jsx
@@ -2,6 +2,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { BsHouse, BsHouseDoor } from "react-icons/bs";
+import { TbCategoryPlus } from "react-icons/tb";
+import { GoPencil } from "react-icons/go";
 
 const HeaderDashboard = () => {
   const [isSticky, setIsSticky] = useState(false);
@@ -43,6 +46,73 @@ const HeaderDashboard = () => {
               className={`${isSticky ? "h-[22px]" : "h-[30px]"} auto hidden`}
             />
           </Link>
+          {/* Navbar menu*/}
+          <div>
+            <ul className="flex items-center gap-7">
+              <li>
+                <Link
+                  href={"/dashboard"}
+                  className="text-15 text-[#595d69] py-4 hover:text-primary flex items-center transition-all ease-in-out font-medium gap-1"
+                >
+                  <BsHouseDoor />
+                  Dashboard
+                </Link>
+              </li>
+              <li className="relative group">
+                <Link
+                  href={"/dashbaord/post"}
+                  className="text-15 text-[#595d69] py-4 hover:text-primary flex items-center transition-all ease-in-out font-medium gap-1"
+                >
+                  <GoPencil />
+                  Blogs
+                </Link>
+                <ul className=" absolute invisible top-[120%] rounded-lg p-5 shadow-lg bg-white block group-hover:top-full opacity-0  group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out">
+                  <li>
+                    <Link
+                      href={"/dashboard/post"}
+                      className="block text-nowrap text-15 text-[#595d69] py-1 hover:text-primary"
+                    >
+                      Blog List
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/dashboard/post/create"}
+                      className="block text-nowrap text-15 text-[#595d69] py-1 hover:text-primary"
+                    >
+                      Create Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/dashboard/post-category"}
+                      className="block text-nowrap text-15 text-[#595d69] py-1 hover:text-primary"
+                    >
+                      Blog Categories
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href={"/dashboard/post-category/create"}
+                      className="block text-nowrap text-15 text-[#595d69] py-1 hover:text-primary"
+                    >
+                      Create Category
+                    </Link>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <Link
+                  href={"/dashboard/post-category"}
+                  className="text-15 text-[#595d69] py-4 hover:text-primary flex items-center transition-all ease-in-out font-medium gap-1"
+                >
+                  <TbCategoryPlus />
+                  Category
+                </Link>
+              </li>
+            </ul>
+          </div>
+
           {/* Right menu */}
           <div className="flex items-center gap-4">Right</div>
         </div>
