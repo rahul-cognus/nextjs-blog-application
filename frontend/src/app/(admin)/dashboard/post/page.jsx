59ccb945b9af44716c3f3ce5834939103e6c8e96
Rahul Jangid <rahul.jangid.cognus@gmail.com>
2024-10-09 18:02:08 +0530

    Changes

--- frontend/src/app/(admin)/dashboard/post/page.jsx
+++ frontend/src/app/(admin)/dashboard/post/page.jsx
@@ -1,12 +1,49 @@
+"use client";
 import TitleHeader from "@/components/dashboard/TitleHeader";
 import { FaCircle, FaSearch } from "react-icons/fa";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { Button, Tooltip } from "@nextui-org/react";
 import { FaRegTrashCan } from "react-icons/fa6";
 import { BsPencilSquare } from "react-icons/bs";
+import { fetchData } from "@/lib/website";
+import { toast } from "react-toastify";
 
 const page = () => {
+  const [blogsData, setBlogsData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const limit = 10;
+  // Fetch categories on component mount
+  useEffect(() => {
+    // Fetch the blogs from the API
+    const fetchBlogs = async () => {
+      try {
+        const response = await fetchData(
+          "/blog/all-blog?page=${currentPage}&limit=${limit}"
+        );
+
+        console.log("data", response);
+        if (response.error) {
+          setError(response.error);
+          toast.error(response.error);
+        } else {
+          setBlogsData(response.blogs);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching blogs:", error);
+        setLoading(false);
+        toast.error(error);
+      }
+    };
+    fetchBlogs();
+  }, [currentPage]);
+
+  if (loading) {
+    return <p>Loading blogs...</p>;
+  }
   return (
     <div className="container">
       <TitleHeader
@@ -47,133 +84,143 @@ const page = () => {
             </select>
           </div>
         </div>
-        <div class="relative flex flex-col w-full h-full overflow-auto text-gray-700 bg-white rounded-lg bg-clip-border">
-          <table class="w-full text-left table-auto min-w-max">
+        <div className="relative flex flex-col w-full h-full overflow-auto text-gray-700 bg-white rounded-lg bg-clip-border">
+          <table className="w-full text-left table-auto min-w-max">
             <thead>
               <tr>
-                <th class="p-4 border-b border-slate-200 bg-[#212529]">
-                  <p class="text-15 font-semibold leading-none text-white">
+                <th className="p-4 border-b border-slate-200 bg-[#212529]">
+                  <p className="text-15 font-semibold leading-none text-white">
                     Post Name
                   </p>
                 </th>
-                <th class="p-4 border-b border-slate-200 bg-[#212529]">
-                  <p class="text-15 font-semibold leading-none text-white">
+                <th className="p-4 border-b border-slate-200 bg-[#212529]">
+                  <p className="text-15 font-semibold leading-none text-white">
                     Author Name
                   </p>
                 </th>
-                <th class="p-4 border-b border-slate-200 bg-[#212529]">
-                  <p class="text-15 font-semibold leading-none text-white">
+                <th className="p-4 border-b border-slate-200 bg-[#212529]">
+                  <p className="text-15 font-semibold leading-none text-white">
                     Published Date
                   </p>
                 </th>
-                <th class="p-4 border-b border-slate-200 bg-[#212529]">
-                  <p class="text-15 font-semibold leading-none text-white">
+                <th className="p-4 border-b border-slate-200 bg-[#212529]">
+                  <p className="text-15 font-semibold leading-none text-white">
                     Category
                   </p>
                 </th>
-                <th class="p-4 border-b border-slate-200 bg-[#212529]">
-                  <p class="text-15 font-semibold leading-none text-white">
+                <th className="p-4 border-b border-slate-200 bg-[#212529]">
+                  <p className="text-15 font-semibold leading-none text-white">
                     Status
                   </p>
                 </th>
-                <th class="p-4 border-b border-slate-200 bg-[#212529]">
-                  <p class="text-15 font-semibold leading-none text-white">
+                <th className="p-4 border-b border-slate-200 bg-[#212529]">
+                  <p className="text-15 font-semibold leading-none text-white">
                     Action
                   </p>
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr class="hover:bg-slate-50 border-b border-slate-200">
-                <td class="p-2">
-                  <Link
-                    href={"/"}
-                    class="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
-                  >
-                    12 worst types of business accounts you follow on Twitter
-                  </Link>
-                </td>
-                <td class="p-2">
-                  <Link
-                    href={"/"}
-                    class="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
-                  >
-                    Lori Stevens
-                  </Link>
-                </td>
-                <td class="p-2">
-                  <p class="text-15 text-[#595d69] hover:text-[#191a1f]">
-                    {" "}
-                    Jan 22, 2022
-                  </p>
-                </td>
-                <td class="p-2">
-                  <Link
-                    href={"/"}
-                    className=" bg-warning flex items-center gap-2 py-1 px-2 rounded-md text-black text-[13px] w-fit"
-                  >
-                    <FaCircle className=" text-xs" />
-                    Technology
-                  </Link>
-                </td>
-                <td class="p-2">
-                  <span className="py-1 px-2 text-success bg-success-50 text-[13px] w-fit rounded-md">
-                    Live
-                  </span>
-                </td>
-                <td class="p-2">
-                  <div className="flex items-center gap-3">
-                    <Tooltip
-                      showArrow={true}
-                      content="Delete"
-                      classNames={{
-                        base: ["before:bg-black"],
-                        content: ["py-2 px-4", "text-white bg-black"],
-                      }}
+              {blogsData.map((blog) => (
+                <tr
+                  key={blog._id}
+                  className="hover:bg-slate-50 border-b border-slate-200"
+                >
+                  <td className="p-2">
+                    <Link
+                      href={`${blog.slug}`}
+                      className="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
                     >
-                      <Button className="shadow min-w-10 w-10 h-10 rounded-full p-0 bg-[#f7f8f9] hover:bg-[#d2d3d4]">
-                        <FaRegTrashCan />
-                      </Button>
-                    </Tooltip>
-                    <Tooltip
-                      showArrow={true}
-                      content="Edit"
-                      classNames={{
-                        base: ["before:bg-black"],
-                        content: ["py-2 px-4", "text-white bg-black"],
-                      }}
+                      {blog.title}
+                    </Link>
+                  </td>
+                  <td className="p-2">
+                    <Link
+                      href={`/author/${blog?.author?._id}`}
+                      className="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
                     >
-                      <Button className="shadow min-w-10 w-10 h-10 rounded-full p-0 bg-[#f7f8f9] hover:bg-[#d2d3d4]">
-                        <BsPencilSquare />
-                      </Button>
-                    </Tooltip>
-                  </div>
-                </td>
-              </tr>
-              <tr class="hover:bg-slate-50 border-b border-slate-200">
-                <td class="p-2">
+                      {blog?.author?.name}
+                    </Link>
+                  </td>
+                  <td className="p-2">
+                    <p className="text-15 text-[#595d69] hover:text-[#191a1f]">
+                      {" "}
+                      {new Date(blog.createdAt).toLocaleDateString("en-US", {
+                        year: "numeric",
+                        month: "short",
+                        day: "numeric",
+                      })}
+                    </p>
+                  </td>
+                  <td className="p-2">
+                    <Link
+                      href={`/category/${blog?.category?._id}`}
+                      className=" bg-warning flex items-center gap-2 py-1 px-2 rounded-md text-black text-[13px] w-fit"
+                    >
+                      <FaCircle className=" text-xs" />
+                      {blog.category.name}
+                    </Link>
+                  </td>
+                  <td className="p-2">
+                    <span className="py-1 px-2 text-success bg-success-50 text-[13px] w-fit rounded-md">
+                      {blog.status}
+                    </span>
+                  </td>
+                  <td className="p-2">
+                    <div className="flex items-center gap-3">
+                      <Tooltip
+                        showArrow={true}
+                        content="Delete"
+                        classNames={{
+                          base: ["before:bg-black"],
+                          content: ["py-2 px-4", "text-white bg-black"],
+                        }}
+                      >
+                        <Button className="shadow min-w-10 w-10 h-10 rounded-full p-0 bg-[#f7f8f9] hover:bg-[#d2d3d4]">
+                          <FaRegTrashCan />
+                        </Button>
+                      </Tooltip>
+                      <Tooltip
+                        showArrow={true}
+                        content="Edit"
+                        classNames={{
+                          base: ["before:bg-black"],
+                          content: ["py-2 px-4", "text-white bg-black"],
+                        }}
+                      >
+                        <Button className="shadow min-w-10 w-10 h-10 rounded-full p-0 bg-[#f7f8f9] hover:bg-[#d2d3d4]">
+                          <BsPencilSquare />
+                        </Button>
+                      </Tooltip>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+
+              {/* <tr className="hover:bg-slate-50 border-b border-slate-200">
+                <td className="p-2">
                   <Link
                     href={"/"}
-                    class="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
+                    className="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
                   >
                     Dirty little secrets about the business industry
                   </Link>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <Link
                     href={"/"}
-                    class="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
+                    className="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
                   >
                     Dennis Barrett
                   </Link>
                 </td>
-                <td class="p-2">
-                  <p class="text-15 text-[#595d69] hover:text-[#191a1f]">
+                <td className="p-2">
+                  <p className="text-15 text-[#595d69] hover:text-[#191a1f]">
                     {" "}
                     Jan 19, 2022
                   </p>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <Link
                     href={"/"}
                     className=" bg-info flex items-center gap-2 py-1 px-2 rounded-md text-white text-[13px] w-fit"
@@ -182,12 +229,12 @@ const page = () => {
                     Marketing
                   </Link>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <span className="py-1 px-2 text-warning bg-warning-50 text-[13px] w-fit rounded-md">
                     Draft
                   </span>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <div className="flex items-center gap-3">
                     <Tooltip
                       showArrow={true}
@@ -216,30 +263,30 @@ const page = () => {
                   </div>
                 </td>
               </tr>
-              <tr class="hover:bg-slate-50 border-b border-slate-200">
-                <td class="p-2">
+              <tr className="hover:bg-slate-50 border-b border-slate-200">
+                <td className="p-2">
                   <Link
                     href={"/"}
-                    class="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
+                    className="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
                   >
                     Dirty little secrets about the business industry
                   </Link>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <Link
                     href={"/"}
-                    class="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
+                    className="block font-medium text-15 text-[#191a1f] hover:text-primary transition-all duration-300"
                   >
                     Dennis Barrett
                   </Link>
                 </td>
-                <td class="p-2">
-                  <p class="text-15 text-[#595d69] hover:text-[#191a1f]">
+                <td className="p-2">
+                  <p className="text-15 text-[#595d69] hover:text-[#191a1f]">
                     {" "}
                     Jan 19, 2022
                   </p>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <Link
                     href={"/"}
                     className=" bg-danger-600 flex items-center gap-2 py-1 px-2 rounded-md text-white text-[13px] w-fit"
@@ -248,12 +295,12 @@ const page = () => {
                     Photography
                   </Link>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <span className="py-1 px-2 text-danger-600 bg-danger-50 text-[13px] w-fit rounded-md">
                     Removed
                   </span>
                 </td>
-                <td class="p-2">
+                <td className="p-2">
                   <div className="flex items-center gap-3">
                     <Tooltip 
                       showArrow={true}
@@ -281,28 +328,28 @@ const page = () => {
                     </Tooltip>
                   </div>
                 </td>
-              </tr>
+              </tr> */}
             </tbody>
           </table>
 
-          <div class="flex justify-between items-center px-4 py-3">
-            <div class="text-sm text-slate-500">
+          <div className="flex justify-between items-center px-4 py-3">
+            <div className="text-sm text-slate-500">
               Showing <b>1-5</b> of 45
             </div>
-            <div class="flex space-x-1">
-              <button class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+            <div className="flex space-x-1">
+              <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                 Prev
               </button>
-              <button class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
+              <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-white bg-slate-800 border border-slate-800 rounded hover:bg-slate-600 hover:border-slate-600 transition duration-200 ease">
                 1
               </button>
-              <button class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+              <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                 2
               </button>
-              <button class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+              <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                 3
               </button>
-              <button class="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
+              <button className="px-3 py-1 min-w-9 min-h-9 text-sm font-normal text-slate-500 bg-white border border-slate-200 rounded hover:bg-slate-50 hover:border-slate-400 transition duration-200 ease">
                 Next
               </button>
             </div>
