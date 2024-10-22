67a69260dcd2f2fd795fd33d8ad88d6b17b8451a
Himanshu.suthar <rahul.jangid.cognus@gmail.com>
2024-10-23 00:16:12 +0530

    Changes

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,5 +1,6 @@
 "use server";
 import { NextResponse, userAgent } from "next/server";
+import { fetchData } from "./lib/website";
 
 export default middleware = async (request) => {
   const url = request.nextUrl;
@@ -17,5 +18,33 @@ export default middleware = async (request) => {
     path: "/",
   });
   response.headers.append("device", viewport);
-  return response;
+
+  // Get token from cookies
+  const token = request.cookies.get("_token");
+  // Check if token exists
+  if (!token) {
+    return NextResponse.redirect(new URL("/login", request.url)); // Redirect to login if no token
+  }
+  // admin middleware
+  // Fetch user data from your API
+  const res = await fetchData("/user/getUserData", "GET");
+
+  console.log("next middleware", res);
+  // Handle the API response
+  if (!res.success) {
+    return NextResponse.redirect(new URL("/login", request.url)); // Redirect if token is invalid or API fails
+  }
+
+  // Check user role
+  if (res.success && res.user.role === "admin") {
+    console.log("res success", res.user);
+    return response; // Allow access to the dashboard
+  } else {
+    return NextResponse.redirect(new URL("/unauthorized", request.url)); // Redirect if not admin
+  }
+};
+
+// Apply the middleware to specific routes using config
+export const config = {
+  matcher: ["/dashboard/:path*", "/dashboard"], // Specify the routes to protect
 };
